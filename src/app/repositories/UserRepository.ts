39c8055966d3cdf08871ed1c2765f62083f5ef5e
gryann <contatogrs03@gmail.com>
2022-08-21 17:26:28 -0300

    Implementação do repositorio criar usuario

---
+++ src/app/repositories/UserRepository.ts
@@ -0,0 +1,18 @@
+import { getRepository, Repository } from "typeorm";
+import User from "../models/User";
+
+class UserRepository {
+
+    private db: Repository<User>;
+
+    constructor() {
+        this.db = getRepository(User);
+    }
+
+    public async create(user: User): Promise<User> {
+        const data = this.db.create(user);
+        await this.db.save(data);
+        return data;
+    }
+    
+}
