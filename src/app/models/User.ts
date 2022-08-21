23c9d27761cb1f3293415846296cff356153eb57
gryann <contatogrs03@gmail.com>
2022-08-21 17:16:29 -0300

    Implementação da model da tabela usuario

---
+++ src/app/models/User.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, BeforeUpdate, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from 'uuid';
+
+@Entity('users')
+class User {
+    @PrimaryColumn()
+    id: string;
+    @Column()
+    name: string
+    @Column()
+    date_birth: Date;
+    @Column()
+    email: string;
+    @Column()
+    telephone_number: string;
+    
+    @Column({name: 'created_at'})
+    createdAt: Date;
+    @Column({name: 'alterated_at'})
+    alteratedAt: Date;
+
+    @BeforeUpdate()
+    updateUser() {
+        this.alteratedAt = new Date();
+    }
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
+}
+
+export default User;
